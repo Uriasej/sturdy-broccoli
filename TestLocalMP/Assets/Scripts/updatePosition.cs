26e2772ec3fd09f6de13bf5803d9bb9cf16f8f25
Uriasej <uriasej@owensborocatholic.us>
2022-01-24 12:22:12 -0600

    Added a display the difference between the ball's y and the lava y with a reset if ball.y < 0

--- TestLocalMP/Assets/Scripts/updatePosition.cs
+++ TestLocalMP/Assets/Scripts/updatePosition.cs
@@ -9,6 +9,12 @@ public class updatePosition : MonoBehaviour
 
     private GameObject ball;
 
+    private Vector3 ballStartPos;
+
+    private bool displayPos;
+
+    private float ballY;
+
     private float lavaY;
 
     private Vector2 lavaPos;
@@ -18,17 +24,47 @@ public class updatePosition : MonoBehaviour
 
     void Start()
     {
-        ball = gameObject;
+        ball = GameObject.Find("Circle").gameObject;
 
         lava = GameObject.Find("lava").gameObject;
 
         lavaPos = lava.transform.position;
 
+        ballStartPos = ball.transform.position;
+
         lavaY = lavaPos.y;
+
+        displayPos = true;
     }
 
     void Update()
     {
-        distanceFromLava = Vector2.Distance(lavaPos, ball.transform.position);
+        ballY = ball.transform.position.y;
+        distanceFromLavaY = ballY - lavaY;
+
+        if (displayPos == true)
+        {
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = "Ball distance from lava: " + distanceFromLavaY;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (distanceFromLavaY < 0)
+        {
+            displayPos = false;
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = "Resetting Ball";
+            new WaitForSeconds(1);
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = "Resetting Ball.";
+            new WaitForSeconds(1);
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = "Resetting Ball..";
+            new WaitForSeconds(1);
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = "Resetting Ball...";
+
+            new WaitForSeconds(1);
+
+            ball.transform.position = ballStartPos;
+            displayPos = true;
+        }
     }
 }
