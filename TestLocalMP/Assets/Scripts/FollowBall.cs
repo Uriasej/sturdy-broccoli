af5d23e880c3930440f5005294e9c1f400266453
Uriasej <uriasej@owensborocatholic.us>
2022-01-26 18:21:59 -0600

    DualShock 4 on Android is working!! Added Bloom toggle (WIP). Added DualShock 4 HID Alt controls for moving. Testing joysticks for next commit.

--- TestLocalMP/Assets/Scripts/FollowBall.cs
+++ TestLocalMP/Assets/Scripts/FollowBall.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class FollowBall : MonoBehaviour
 {
+
+    private PostProcessVolume postProcessVolume;
+
+    private Bloom bloom;
+
+    private Vignette vignette;
     public GameObject ball;
     public Vector2 ballPos;
 
+    private bool bloomEnabled;
+    private bool vignetteEnabled;
+
     void Start()
     {
+        bloomEnabled = true;
+
+        vignetteEnabled = true;
+
         ball = GameObject.Find("Circle");
+
+        postProcessVolume = gameObject.GetComponent<PostProcessVolume>();
+
+        postProcessVolume.profile.TryGetSettings(out bloom);
+        postProcessVolume.profile.TryGetSettings(out vignette);
+
     }
 
     void Update()
@@ -17,5 +37,36 @@ public class FollowBall : MonoBehaviour
         ballPos = ball.transform.position;
 
         gameObject.transform.position = new Vector3(ballPos.x, ballPos.y, -100);
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(1000, 10, 200, 30), "Bloom Toggle"))
+        {
+            if (bloomEnabled == false)
+            {
+                bloom.active = true;
+                bloomEnabled = true;
+            }
+            else if (bloomEnabled == true)
+            {
+                bloom.active = false;
+                bloomEnabled = false;
+            }
+        }
+
+        if (GUI.Button(new Rect(1000, 40, 200, 30), "Vignette Toggle"));
+        {
+            if (vignetteEnabled == false)
+            {
+                vignette.active = true;
+                vignetteEnabled = true;
+            }
+            else if (vignetteEnabled == true)
+            {
+                vignette.active = false;
+                vignetteEnabled = false;
+            }
+        }
     }
 }
